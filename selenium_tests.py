7f5316244c1fe6766522894b2538467e6a09d1f9
Rogerio Morais <rogeriosmorais@gmail.com>
2020-06-23 14:24:29 -0300

    Add files via upload

---
+++ selenium_tests.py
@@ -0,0 +1,100 @@
+Skip to content
+Search or jump to…
+
+Pull requests
+Issues
+Marketplace
+Explore
+ 
+@rogeriosmorais 
+rogeriosmorais
+/
+selenium-azure
+1
+00
+Code
+Issues
+Pull requests
+Actions
+Projects
+Wiki
+Security
+Insights
+Settings
+selenium-azure/selenium_tests.py /
+@rogeriosmorais
+rogeriosmorais Rename selenium.py to selenium_tests.py
+Latest commit d5e4176 5 days ago
+ History
+ 1 contributor
+57 lines (50 sloc)  2.88 KB
+  
+import sys
+sys.path.insert(0,'/usr/lib/chromium-browser/chromedriver')
+sys.path.insert(0,'/home/vsts/work/node_modules/chromedriver/lib/chromedriver/chromedriver')
+sys.path.insert(0,'/home/vsts/work/node_modules/chromedriver/lib/chromedriver/')
+from selenium import webdriver
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+
+import time
+
+def login (user, password):
+    print ('Starting the browser...')
+    # --uncomment when running in Azure DevOps.
+    #options = webdriver.ChromeOptions()
+    #options.add_argument("--headless") 
+    #driver = webdriver.Chrome(options=options)
+    driver = webdriver.Chrome('chromedriver', options=chrome_options)
+    print ('Browser started successfully. Navigating to the demo page to login.')
+    driver.get('https://www.saucedemo.com/')
+    driver.find_element_by_css_selector("input[id='user-name']").send_keys(user)
+    driver.find_element_by_css_selector("input[id='password']").send_keys(password)
+    driver.find_element_by_css_selector("input[class='btn_action']").click()
+    time.sleep(2)
+    #driver.save_screenshot("screenshot.png")
+    our_search = driver.find_element_by_css_selector("div[class=product_label]").text
+    assert "Products" in our_search
+    print("User {} logged in successfuly".format(user))
+    return driver
+
+def addProductsToCart(driver):
+    for inventory_item in driver.find_elements_by_css_selector("div[class='inventory_item']"):    
+      product = inventory_item.find_element_by_css_selector("div[class='inventory_item_label']").text.splitlines()[0]      
+      button = inventory_item.find_element_by_css_selector("div[class='pricebar'] > button[class='btn_primary btn_inventory']")
+      if button.text == 'ADD TO CART':
+        button.click()
+      button = inventory_item.find_element_by_css_selector("div[class='pricebar'] > button[class='btn_secondary btn_inventory']")
+      assert button.text == 'REMOVE'
+      print("Added {} to cart".format(product))    
+
+def removeProductsFromCart(driver):
+    for inventory_item in driver.find_elements_by_css_selector("div[class='inventory_item']"):    
+      product = inventory_item.find_element_by_css_selector("div[class='inventory_item_label']").text.splitlines()[0]      
+      button = inventory_item.find_element_by_css_selector("div[class='pricebar'] > button[class='btn_secondary btn_inventory']")
+      if button.text == 'REMOVE':
+        button.click()
+      button = inventory_item.find_element_by_css_selector("div[class='pricebar'] > button[class='btn_primary btn_inventory']")
+      assert button.text == 'ADD TO CART'
+      print("Removed {} from cart".format(product))
+
+
+driver = login('standard_user', 'secret_sauce')
+addProductsToCart(driver)
+removeProductsFromCart(driver)
+
+print("Finished test execution.")
+© 2020 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Help
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
